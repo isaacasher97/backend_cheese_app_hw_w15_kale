20b19a682e48355063a475740309cf00e6f7d92e
isaacasher97 <isaac.asher97@gmail.com>
2023-08-05 16:37:40 -0400

    connected to mongodb

--- server.js
+++ server.js
@@ -1,8 +1,22 @@
 //Dependencies
 require("dotenv").config();
-const { PORT = 3000 } = process.env;
+const { PORT = 3000, MONGODB_URL } = process.env;
 const express = require("express");
 const app = express();
+//import mongoose
+const mongoose = require("mongoose");
+
+
+//Database connection
+////////////////////////
+//establish connection
+mongoose.connect(MONGODB_URL)
+//connection events
+mongoose.connection
+  .on("open", () => console.log("Your are connected to mongoose"))
+  .on("close", () => console.log("Your are disconnected from mongoose"))
+  .on("error", (error) => console.log(error));
+
 
 //ROUTES
 
