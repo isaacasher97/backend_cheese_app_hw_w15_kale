1c83e001c0a6a77e44c73874d229dc4bbcaf1c60
isaacasher97 <isaac.asher97@gmail.com>
2023-08-05 16:55:44 -0400

    Added cheese model + tested index/create routes in Postman

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const express = require("express");
 const app = express();
 //import mongoose
 const mongoose = require("mongoose");
+//import middleware
+const cors = require("cors");
+const morgan = require("morgan");
 
 
 //Database connection
@@ -17,6 +20,21 @@ mongoose.connection
   .on("close", () => console.log("Your are disconnected from mongoose"))
   .on("error", (error) => console.log(error));
 
+//MODELS
+const CheeseScehma = new mongoose.Schema({
+    name: String,
+    countryOfOrigin: String,
+    image: String
+})
+
+const Cheese = mongoose.model("Cheese", CheeseScehma);
+
+
+
+//Middleware
+app.use(cors()); // prevent cors error
+app.use(morgan('dev')) 
+app.use(express.json())// for parsing json bodies  
 
 //ROUTES
 
@@ -25,6 +43,24 @@ app.get('/', (req, res) => {
     res.json({
         "hello": "world"
     })
+});
+
+//CHEESE INDEX ROUTE - GET
+app.get('/cheese', async (req, res) => {
+    try {
+        res.json(await Cheese.find({}))
+    } catch (error) {
+        res.status(400).json(error);
+    }
+});
+
+//CHEESE CREATE ROUTE - POST
+app.post("/cheese", async (req, res) => {
+    try{
+        res.json(await Cheese.create(req.body))
+    } catch (error) {
+        res.status(400).json(error)
+    }
 });
 
 //Listener
